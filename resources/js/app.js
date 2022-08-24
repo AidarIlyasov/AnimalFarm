b56785eb4ce0a7731022e4b42502a706e4ce2ff0
AidarIlyasov <06moboru@mail.ru>
2022-08-24 19:24:47 +0300

    add vue.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import AnimalButton from './components/AnimalButton.vue';
+
+const app = createApp(App)
+app.component('AnimalButton', AnimalButton);
+
+app.mount('#app')
+
 require('./bootstrap');
